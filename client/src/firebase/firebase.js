151840940150430680a8f06cfcc34ab1c4de8a60
caaal0 <jcsagun@up.edu.ph>
2024-10-02 16:44:24 +0800

    created firebase client instance for authentication

---
+++ client/src/firebase/firebase.js
@@ -0,0 +1,23 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+console.log("FIREBASE SUCCESFULLY LOADED");
+console.log(firebaseConfig.apiKey);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and export it
+const auth = getAuth(app);
+
+export { auth };
