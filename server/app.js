3dd560ff6a8e354bdbb2b92cec7239f38e1dacca
caaal0 <jcsagun@up.edu.ph>
2024-10-02 14:44:05 +0800

    added cors dependency

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import routes from './routes.js';
+import cors from 'cors';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
