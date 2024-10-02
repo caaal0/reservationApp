1928abf0d73779895c45dfa7c66b6d323ef7f1e4
caaal0 <jcsagun@up.edu.ph>
2024-10-02 16:43:47 +0800

    changed backend code for login (verify token)

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -50,16 +50,21 @@ const signup = async (req, res) => {
 
 const login = async (req, res) => {
     try {
-        const { email, password } = req.body;
-        // const userRecord = await admin.auth().getUserByEmail(email);
-        // res.send({ success: true, user: userRecord });
-        const userCredential = await admin.auth().signInWithEmailAndPassword(email, password);
-        const token = await userCredential.user.getIdToken();
-
+        const token = req.headers.authorization?.split(' ')[1]; // Extract token from the header
+    
+        if (!token) {
+          return res.status(401).send({ success: false, msg: 'No token provided' });
+        }
+    
+        // Verify the token using Firebase Admin SDK
+        const decodedToken = await admin.auth().verifyIdToken(token);
         
-    } catch (err) {
+        const user = await admin.auth().getUser(decodedToken.uid); // Get the user data
+        console.log(user);
+        res.status(200).send({ success: true, user: user });
+      } catch (err) {
         res.status(500).send({ success: false, msg: 'Unable to login', error: err.message });
-    }
+      }
 }
 
 export default {
