e5d3e9a6c6d656418e9e35be906397942e06a775
caaal0 <jcsagun@up.edu.ph>
2024-10-03 19:12:56 +0800

    removed console logs

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -11,8 +11,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-console.log("FIREBASE SUCCESFULLY LOADED");
-console.log(firebaseConfig.apiKey);
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
