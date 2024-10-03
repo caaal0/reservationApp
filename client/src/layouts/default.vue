be6b848f74da4de2baa786e24059c6735fc13a79
caaal0 <jcsagun@up.edu.ph>
2024-10-03 19:14:30 +0800

    added user greeting and menu with logout

--- client/src/layouts/default.vue
+++ client/src/layouts/default.vue
@@ -1,3 +1,51 @@
+<script setup>
+  import AppFooter from '@/components/AppFooter.vue';
+  import LoginForm from '@/components/LoginForm.vue';
+  import SignupForm from '@/components/SignupForm.vue';
+  import { useRouter } from 'vue-router';
+  import { auth } from '../firebase/firebase.js';
+  import { signOut } from 'firebase/auth';
+
+  const router = useRouter();
+  const showLogin = ref(false);
+  const showSignup = ref(false);
+  const drawer = ref(false);
+  const isLoggedIn = ref(false);
+
+  const items = [
+    { title: 'Edit information', icon: 'mdi-pencil' },
+    { title: 'Logout', icon: 'mdi-logout', click: logout },
+  ]
+
+  function switchToSignup() {
+    showLogin.value = false;
+    showSignup.value = true;
+  }
+
+  function switchToLogin() {
+    showSignup.value = false;
+    showLogin.value = true;
+  }
+
+  function loggedIn() {
+    isLoggedIn.value = true;
+    showLogin.value = false;
+    //in the case that the user logs in from the signup form
+    if(showSignup.value) showSignup.value = false;
+  }
+
+  function logout(){
+    try{
+      signOut(auth);
+      isLoggedIn.value = false;
+      alert('Logout successful');
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Logout error.');
+    }
+  }
+</script>
+
 <template>
   <v-app>
 
@@ -6,14 +54,37 @@
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       </template>
       <!-- <v-app-bar-title class="appbarTitle" >Seated</v-app-bar-title> -->
-      <v-toolbar-title
+      <v-app-bar-title
         class="appbarTitle"
         @click="() => router.push('/')"
-        >Seated</v-toolbar-title>
+        >Seated</v-app-bar-title>
       <v-spacer />
-      <v-btn text to="/">Home</v-btn>
-      <v-btn text @click="showLogin = true">Login</v-btn>
-      <v-btn text @click="showSignup = true">Signup</v-btn>
+      <!-- <v-btn text to="/">Home</v-btn> -->
+      <div v-if="!isLoggedIn">
+        <v-btn text @click="showLogin = true">Login</v-btn>
+        <v-btn text @click="showSignup = true">Signup</v-btn>
+      </div>
+      <div v-else>
+         <!-- greeting and menu -->
+         <span id="greeting">Hi {{auth.currentUser.displayName}}!</span>
+         <v-menu>
+          <template v-slot:activator="{ props }">
+            <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+          </template>
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              @click="item.click"
+            >
+              <template v-slot:prepend>
+                <v-icon>{{ item.icon }}</v-icon>
+              </template>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+         </v-menu>
+      </div>
     </v-app-bar>
 
     <v-navigation-drawer
@@ -48,11 +119,11 @@
     </v-navigation-drawer>
 
     <v-dialog v-model="showLogin" max-width="500px" transition="dialog-top-transition" persistent>
-      <LoginForm @close="showLogin = false" @switch-to-signup="switchToSignup" />
+      <LoginForm @close="showLogin = false" @switch-to-signup="switchToSignup" @login-success="loggedIn" />
     </v-dialog>
 
     <v-dialog v-model="showSignup" max-width="500px" transition="dialog-top-transition" persistent>
-      <SignupForm @close="showSignup = false" @switch-to-login="switchToLogin" />
+      <SignupForm @close="showSignup = false" @switch-to-login="switchToLogin" @signup-success="loggedIn"/>
     </v-dialog>
 
     <v-main>
@@ -62,28 +133,6 @@
     <!-- <AppFooter /> -->
   </v-app>
 </template>
-
-<script setup>
-  import AppFooter from '@/components/AppFooter.vue';
-  import LoginForm from '@/components/LoginForm.vue';
-  import SignupForm from '@/components/SignupForm.vue';
-  import { useRouter } from 'vue-router';
-
-  const router = useRouter();
-  const showLogin = ref(false);
-  const showSignup = ref(false);
-  const drawer = ref(false);
-
-  function switchToSignup() {
-    showLogin.value = false;
-    showSignup.value = true;
-  }
-
-  function switchToLogin() {
-    showSignup.value = false;
-    showLogin.value = true;
-  }
-</script>
 
 <style scoped>
 .no-background {
@@ -116,5 +165,12 @@
   cursor: pointer;
   max-width: fit-content;
   display: inline-block;
+}
+#greeting {
+  font-size: 1.25rem;
+  font-weight: bold;
+  color: var(--black);
+  align-self: center;
+  margin: 0 1rem 0 1rem;
 }
 </style>
