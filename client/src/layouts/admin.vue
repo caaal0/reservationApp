277fd543de2abefe91e023e4d267e19b4b365b0c
caaal0 <jcsagun@up.edu.ph>
2024-10-08 14:06:49 +0800

    new layouts

---
+++ client/src/layouts/admin.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar title="Admin">
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
---
+++ client/src/layouts/staff.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from 'vue'
+</script>
+
+<template>
+  <v-app>
+    <v-app-bar title="Staff">
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
