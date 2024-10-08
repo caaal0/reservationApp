8d3eb2f49117d5777f44de786462f46671542d85
caaal0 <jcsagun@up.edu.ph>
2024-10-08 14:07:03 +0800

    my reservations page for users

---
+++ client/src/pages/myreservations.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <v-container class="fill-height">
+    <v-row justify="center">
+      <v-col class="text-center">
+        <h1>My Reservations</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
