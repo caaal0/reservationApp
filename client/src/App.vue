0aa623b354b19be1cc2b7cd382626db3eb51eef1
caaal0 <jcsagun@up.edu.ph>
2024-10-08 14:03:50 +0800

    refactored to accommodate different layouts

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,12 @@
 <template>
-  <v-app>
-    <v-main>
-      <router-view />
-    </v-main>
-  </v-app>
+  <RouterView v-slot="{ Component, layout }">
+    <component :is="layout || 'default'">
+      <component :is="Component" />
+    </component>
+  </RouterView>
 </template>
 
 <script setup>
-  //
+import { useRoute } from 'vue-router'
+const route = useRoute()
 </script>
