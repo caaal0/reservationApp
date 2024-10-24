571c42ebf15ad22a80753245c5b7b49c5d406c4a
caaal0 <jcsagun@up.edu.ph>
2024-10-25 02:52:22 +0800

    added required authentication for admin and staff routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,22 +8,25 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
+import { useAuthStore } from '../stores/auth'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../firebase/firebase'
 
 // Modify routes to set layout based on path pattern
 const updatedRoutes = routes.map((route) => {
   if (route.path.startsWith('/admin')) {
-    route.meta = { layout: 'admin' } // Use Admin layout
+    route.meta = { layout: 'admin', requiresAuth: true, role: 'admin' } // Set meta for layout, auth, and role
   } else if (route.path.startsWith('/staff')) {
-    route.meta = { layout: 'staff' } // Use Staff layout
+    route.meta = { layout: 'staff', requiresAuth: true, role: 'staff' }
   } else {
-    route.meta = { layout: 'default' } // Use Default layout
+    route.meta = { layout: 'default' }
   }
   return route
 })
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: setupLayouts(routes),
+  routes: setupLayouts(updatedRoutes),
 })
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
@@ -40,6 +43,40 @@ router.onError((err, to) => {
     console.error(err)
   }
 })
+
+// Global navigation guard
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore();
+  authStore.fetchCurrentUser();
+  const user = authStore.user;
+  // console.log('User currently logged in:', user);
+  // console.log('User role: ', authStore.userRole);
+
+  // Check if the route requires authentication
+  if (to.meta.requiresAuth) {
+    if (!user) {
+      // Avoid redirecting to login if already on the login page
+      if (to.path !== '/admin/login') {
+        return next('/admin/login'); // Redirect to login if not authenticated
+      } else {
+        return next(); // Continue if already on the login page
+      }
+    }
+
+    const userRole = authStore.userRole;
+
+    // If the route requires a specific role and the user doesn't match, redirect
+    if (to.meta.role && to.meta.role !== userRole) {
+      alert('Unauthorized access');
+      return next('/admin/login'); // Redirect unauthorized users
+    }else{
+      return next(); // Proceed if the user is authenticated
+    }
+  }
+
+  // Proceed to the next route if everything is fine
+  next();
+});
 
 router.isReady().then(() => {
   localStorage.removeItem('vuetify:dynamic-reload')
