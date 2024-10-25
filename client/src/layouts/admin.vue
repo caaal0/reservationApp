a7985436016255a8979d2d2b247af57e52b7a133
caaal0 <jcsagun@up.edu.ph>
2024-10-26 01:59:44 +0800

    fixed logout; added back icon

--- client/src/layouts/admin.vue
+++ client/src/layouts/admin.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { useRouter, useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth';
 import { auth } from '../firebase/firebase.js';
 import { signOut} from 'firebase/auth';
 
-const isLoggedIn = ref(false);
 const router = useRouter();
+const currentRoute = useRoute();
 const authStore = useAuthStore();
 authStore.fetchCurrentUser();
-
+console.log(currentRoute)
 const unauthorizedSnackbar = ref(false);
 
 if (!authStore.isAuthenticated() || authStore.userRole !== 'admin') {
@@ -19,8 +19,11 @@ if (!authStore.isAuthenticated() || authStore.userRole !== 'admin') {
 function logout() {
   signOut(auth);
   authStore.clearUser();
-  isLoggedIn.value = false;
   router.push('/admin/login');
+}
+
+function back() {
+  router.back();
 }
 
 </script>
@@ -28,13 +31,16 @@ function logout() {
 <template>
   <v-app>
     <v-app-bar app color="white" elevation="1">
+      <template v-slot:prepend v-if="currentRoute.path != '/admin' && currentRoute.path != '/admin/login'">
+        <v-icon @click="back" icon="mdi-chevron-left" style="padding-left: 10px"></v-icon>
+      </template>
       <v-app-bar-title
-      class="appbar-title"
+      class="appBar-title"
       @click="() => router.push('/admin')"
-      >Seated - Admin
+      >Seated
       </v-app-bar-title>
       <v-spacer></v-spacer>
-      <v-btn text v-if="isLoggedIn" @click="logout">
+      <v-btn text v-if="authStore.userRole == 'admin'" @click="logout">
         <v-icon>mdi-logout</v-icon>
         Logout
       </v-btn>
@@ -51,7 +57,7 @@ function logout() {
 </template>
 
 <style scoped>
-.appbar-title {
+.appBar-title {
   color: green;
   margin: '0 0.25rem 1rem 0.25rem';
   font-size: 2.25rem;
@@ -60,4 +66,5 @@ function logout() {
   max-width: fit-content;
   display: inline-block;
 }
+
 </style>
