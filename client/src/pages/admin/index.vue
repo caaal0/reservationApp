db05c123a2a97c9c7b1d99e9d2fe1723e3972300
caaal0 <jcsagun@up.edu.ph>
2024-10-29 20:50:13 +0800

    fixed route pushing

--- client/src/pages/admin/index.vue
+++ client/src/pages/admin/index.vue
@@ -43,7 +43,7 @@ import { onMounted, ref } from 'vue'
         </v-card>
       </v-col>
       <v-col cols="12" md="3" sm="6">
-        <v-card hover to="/admin/statistics" image="..\..\assets\vecteezy_simple-and-modern-style-flat-design-edit-icon-isolated-on-a_7164151.svg">
+        <v-card hover to="/admin/walkins" image="..\..\assets\vecteezy_simple-and-modern-style-flat-design-edit-icon-isolated-on-a_7164151.svg">
           <v-card-title>Walk-ins</v-card-title>
           <v-card-text>
 
