5e71c39a803c7de9f9292735d29a97e9f08727a2
caaal0 <jcsagun@up.edu.ph>
2024-11-05 00:14:00 +0800

    firebase cred and cors link

--- server/app.js
+++ server/app.js
@@ -5,12 +5,12 @@ import cors from 'cors';
 import { updateSeatAvailabilityJob, clearCurrentReservationJob } from './scheduler.js';
 
 const app = express();
-app.use(cors());
-// app.use(cors({
-//     origin: ['https://seated-kw2aprjyj-justin-carl-saguns-projects.vercel.app'],
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-// }));
+// app.use(cors());
+app.use(cors({
+    origin: ['https://seated-qoy8viylm-justin-carl-saguns-projects.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
