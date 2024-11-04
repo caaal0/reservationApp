b6a3ddd235dd1b69afdebecf8e394d7932991da9
caaal0 <jcsagun@up.edu.ph>
2024-11-04 21:28:28 +0800

    remove hardcode

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 import app from './app.js';
 import dotenv from 'dotenv';
-import db from './firebase.js';
+
 
 dotenv.config();
 
 app.listen( process.env.PORT || 8080 , () => {
-    console.log('Server is running on http://localhost:8080');
+    console.log(`Server is running on ${process.env.PORT || 8080}`);
 });
