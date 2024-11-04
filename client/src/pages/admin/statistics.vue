d2202a6fdaa5a787b07d4f3b8737e6c715f130dd
caaal0 <jcsagun@up.edu.ph>
2024-11-04 20:54:43 +0800

    boilerplate

--- client/src/pages/admin/statistics.vue
+++ client/src/pages/admin/statistics.vue
@@ -1,9 +1,21 @@
 <script setup>
-
+import { onMounted, ref } from 'vue'
 </script>
 
 <template>
-  <h1>Statistics</h1>
+  <v-container>
+    <h1>Statistics</h1>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-card>
+          <v-card-title>Users</v-card-title>
+          <v-card-text>
+
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
