47d18318c6e52e78da8a438159eff6b9e3187bf2
caaal0 <jcsagun@up.edu.ph>
2024-11-06 16:11:36 +0800

    added router buttons to admin and staff login as well

--- client/src/pages/admin/login.vue
+++ client/src/pages/admin/login.vue
@@ -84,10 +84,29 @@ async function login(){
             type="submit"
             width="100%"
             :loading="loading"
+            rounded="false"
           >
             Login
           </v-btn>
         </v-form>
+        <v-btn
+        width="200px"
+        style="margin-right: 10px; margin-top: 20px; margin-left: 10px;"
+        class="mt-5"
+        color="green-darken-2"
+        rounded="false"
+        to="/"
+        > Back to homepage
+        </v-btn>
+        <v-btn
+        width="200px"
+        style="margin-left: 10px; margin-top: 20px; margin-right: 10px;"
+        class="mt-5"
+        color="green-darken-2"
+        rounded="false"
+        to="/staff/login"
+        > Login as Staff
+        </v-btn>
       </v-col>
     </v-row>
     <v-snackbar v-model="unauthorizedSnackbar" color="red">
