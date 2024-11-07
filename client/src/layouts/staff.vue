1c6933b149891441b8249224ceba8fc579bcd3db
caaal0 <jcsagun@up.edu.ph>
2024-11-08 04:00:05 +0800

    Staff can now retrieve and edit their information from the dashboard

--- client/src/layouts/staff.vue
+++ client/src/layouts/staff.vue
@@ -4,9 +4,13 @@ import { useRouter, useRoute } from 'vue-router'
 import { useAuthStore } from '@/stores/auth';
 import { auth } from '../firebase/firebase.js';
 import { signOut} from 'firebase/auth';
+import { useDisplay } from 'vuetify';
+import EditInformation from '@/components/EditInformation.vue';
 
+const { smAndDown, mdAndUp } = useDisplay();
 const router = useRouter();
 const currentRoute = useRoute();
+const showEditInformationDialog = ref(false);
 const authStore = useAuthStore();
 authStore.fetchCurrentUser();
 const unauthorizedSnackbar = ref(false);
@@ -40,15 +44,47 @@ function back() {
       <img src="../assets/chair-svgrepo-com.svg" alt="chair" style="height: 2rem; margin-right: 0.25rem;"/>
       Seated
       </v-app-bar-title>
-      <v-spacer></v-spacer>
-      <v-btn text v-if="authStore.userRole == 'staff' && currentRoute.path != '/staff/login'" @click="logout">
-        <v-icon>mdi-logout</v-icon>
-        Logout
-      </v-btn>
+      <template v-slot:append>
+        <div v-if="authStore.userRole == 'staff' && currentRoute.path != '/staff/login' && mdAndUp">
+          <v-btn text @click="showEditInformationDialog = true">
+            <v-icon>mdi-account-edit</v-icon>
+            Edit Information
+          </v-btn>
+          <v-btn text  @click="logout">
+            <v-icon>mdi-logout</v-icon>
+            Logout
+          </v-btn>
+        </div>
+        <div v-else-if="authStore.userRole == 'staff' && currentRoute.path != '/staff/login' && smAndDown">
+          <v-menu>
+            <template v-slot:activator="{ props }">
+              <v-btn icon="mdi-dots-vertical" variant="text" v-bind="props"></v-btn>
+            </template>
+            <v-list>
+              <v-list-item @click="showEditInformationDialog = true">
+                <template v-slot:prepend>
+                  <v-icon>mdi-account-edit</v-icon>
+                </template>
+                <v-list-item-title>Edit Information</v-list-item-title>
+              </v-list-item>
+              <v-list-item @click="logout">
+                <template v-slot:prepend>
+                  <v-icon>mdi-logout</v-icon>
+                </template>
+                <v-list-item-title>Logout</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </template>
     </v-app-bar>
 
     <v-main>
       <router-view />
+
+      <v-dialog v-model="showEditInformationDialog" max-width="500px" transition="dialog-top-transition">
+        <EditInformation @close="showEditInformationDialog = false"/>
+      </v-dialog>
     </v-main>
 
     <v-snackbar v-model="unauthorizedSnackbar" color="red">
